6d7594727aeb59bc81f436f0cb1580ed6d54cd06
dryyss <65180701+dryyss@users.noreply.github.com>
2023-10-20 16:33:02 +0200

    💄 F03 Login  page Styling on background

--- src/Features/LoginPage/LoginPage.jsx
+++ src/Features/LoginPage/LoginPage.jsx
@@ -5,12 +5,25 @@ import Logo from "../../Components/Logo/Logo";
 import { theme } from "../../theme";
 
 const LoginPageStyled = styled.div`
-  background: red;
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+
+  ::before {
+    content: "avant";
+    background: url("/assets/burger-background.jpg");
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: darken;
+    z-index: -1;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
 `;
 
 const LoginPage = () => {
@@ -18,7 +31,6 @@ const LoginPage = () => {
     <ThemeProvider theme={theme}>
       <LoginPageStyled>
         <Logo />
-
         <LoginForm />
       </LoginPageStyled>
     </ThemeProvider>
